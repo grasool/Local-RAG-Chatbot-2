c03ac0d590612f7379ba857eaff7855eba3bf04d
grasool <mr.ghulamrasool@gmail.com>
2024-02-09 17:51:08 -0500

    Local RAG Chatbot Using LlamaIndex

---
+++ main.py
@@ -0,0 +1,27 @@
+#main.py
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from llama_index.embeddings import resolve_embed_model
+#from llama_index.llms import Ollama
+
+from llama_index.llms import OpenAI
+
+documents = SimpleDirectoryReader("data").load_data()
+
+# bge-m3 embedding model
+embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
+
+
+llm = OpenAI(temperature=0.0, api_base="http://localhost:1234/v1", api_key="not-needed")
+
+service_context = ServiceContext.from_defaults(
+    embed_model=embed_model, llm=llm
+)
+
+index = VectorStoreIndex.from_documents(
+    documents, service_context=service_context
+)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
+
